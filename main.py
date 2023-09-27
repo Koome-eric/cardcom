65051e611bdefec8189712a5efcda9416af13cb4
Koome-eric <erickomee4195@gmail.com>
2023-09-27 15:26:21 +0300

    second commit

--- main.py
+++ main.py
@@ -1,72 +1,86 @@
+from flask import Flask, render_template_string
 import requests
 import datetime
 
-def determine_tag(amount):
-    # Mapping of amounts and their corresponding tags
-    tags_mapping = {
-        300: "Learning sponsorship",
-        350: "PWI Paid",  # This might conflict since there are two 350 values with different tags in the data you provided.
-        500: "Kiddish Sponsored",
-        2600: "Membership Paid",
-        200: "Membership Paid",  # seat for members
-        1000: "Membership Paid"  # seat for non members
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+
+    def determine_tag(amount):
+        tags_mapping = {
+            300: "Learning sponsorship",
+            350: "PWI Paid",
+            500: "Kiddish Sponsored",
+            2600: "Membership Paid",
+            200: "Membership Paid",
+            1000: "Membership Paid"
+        }
+        return tags_mapping.get(amount, "General donation")
+
+    current_date = datetime.datetime.now().date()
+    previous_day = current_date - datetime.timedelta(days=1)
+
+    transactionsEndpoint = "https://secure.cardcom.solutions/api/v11/Transactions/ListTransactions"
+    webhook_url = "https://hooks.zapier.com/hooks/catch/8868151/39x5zlt/"
+
+    payload = {
+        "ApiName": "1wq46yBe94ZntGBXdohR",
+        "ApiPassword": "gnrBVd8oCM6eou3oeb1x",
+        "TerminalNumber": "34375",
+        "FromDate": previous_day.strftime('%d%m%Y'),
+        "ToDate": previous_day.strftime('%d%m%Y'),
+        "TranStatus": "Success",
+        "Page": 1,
+        "Page_size": 100,
+        "LimitForTerminal": "your_terminal_number_here"
     }
 
-    return tags_mapping.get(amount, "General donation")  # Default to "General donation" if no exact match is found
+    response = requests.post(transactionsEndpoint, json=payload)
+    html_string = "<h1>Extracted Data</h1>"
 
-# Get current date and calculate the previous day
-current_date = datetime.datetime.now().date()
-previous_day = current_date - datetime.timedelta(days=1)
+    if response.status_code == 200:
+        data = response.json()
 
-# API endpoint
-transactionsEndpoint = "https://secure.cardcom.solutions/api/v11/Transactions/ListTransactions"
-webhook_url = "https://hooks.zapier.com/hooks/catch/8868151/39x5zlt/"
+        for transaction in data.get('Tranzactions', []):
+            tag = determine_tag(transaction['Amount'])
 
-payload = {
-    "ApiName": "1wq46yBe94ZntGBXdohR",
-    "ApiPassword": "gnrBVd8oCM6eou3oeb1x",
-    "TerminalNumber": "34375",
-    "FromDate": previous_day.strftime('%d%m%Y'),  # Set to previous day
-    "ToDate": previous_day.strftime('%d%m%Y'),    # Set to previous day
-    "TranStatus": "Success",
-    "Page": 1,
-    "Page_size": 100,
-    "LimitForTerminal": "your_terminal_number_here"
-}
+            html_string += f"""
+            <div>
+                <p><strong>Transaction ID:</strong> {transaction['TranzactionId']}</p>
+                <p><strong>Amount:</strong> {transaction['Amount']}</p>
+                <p><strong>Card Owner Name:</strong> {transaction['CardOwnerName']}</p>
+                <p><strong>Card Owner Email:</strong> {transaction['CardOwnerEmail']}</p>
+                <p><strong>Card Owner Phone:</strong> {transaction['CardOwnerPhone']}</p>
+                <p><strong>Tag:</strong> {tag}</p>
+                <hr>
+            </div>
+            """
+            
+            transaction_data = {
+                "Transaction ID": transaction['TranzactionId'],
+                "Product Purchased": transaction['Description'],
+                "Amount": transaction['Amount'],
+                "Card Owner Name": transaction['CardOwnerName'],
+                "Card Owner Email": transaction['CardOwnerEmail'],
+                "Card Owner Phone": transaction['CardOwnerPhone'],
+                "Transaction Date": transaction['CreateDate'],
+                "Tag": tag
+            }
 
-response = requests.post(transactionsEndpoint, json=payload)
+            webhook_response = requests.post(webhook_url, json=transaction_data)
+            if webhook_response.status_code != 200:
+                html_string += f"""
+                <div>
+                    <p style="color: red;">Failed to send data to webhook for Transaction ID: {transaction['TranzactionId']}. Status Code: {webhook_response.status_code}, Response: {webhook_response.text}</p>
+                </div>
+                """
 
-# Ensure the request was successful
-if response.status_code == 200:
-    data = response.json()
-    
-    # Loop through the transactions
-for transaction in data.get('Tranzactions', []):
-    tag = determine_tag(transaction['Amount'])
-    
-    print(f"Transaction ID: {transaction['TranzactionId']}")
-    print(f"Amount: {transaction['Amount']}")
-    print(f"Card Owner Name: {transaction['CardOwnerName']}")
-    print(f"Card Owner Email: {transaction['CardOwnerEmail']}")
-    print(f"Card Owner Phone: {transaction['CardOwnerPhone']}")
-    print(f"Tag: {tag}")  # Print the determined tag
-    print("--------------")  # Separator for better readability
-    
-    transaction_data = {
-        "Transaction ID": transaction['TranzactionId'],
-        "Product Purchased": transaction['Description'],
-        "Amount": transaction['Amount'],
-        "Card Owner Name": transaction['CardOwnerName'],
-        "Card Owner Email": transaction['CardOwnerEmail'],
-        "Card Owner Phone": transaction['CardOwnerPhone'],
-        "Transaction Date": transaction['CreateDate'],
-        "Tag": tag  # Adding the determined tag
-    }
+    return render_template_string(html_string)
 
-    # Send the transaction data to the webhook
-    webhook_response = requests.post(webhook_url, json=transaction_data)
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "404", "message": "Page not found"}), 404
 
-    # Optional: check the webhook response
-    if webhook_response.status_code != 200:
-        print(f"Failed to send data to webhook for Transaction ID: {transaction['TranzactionId']}. Status Code: {webhook_response.status_code}, Response: {webhook_response.text}")
-
+if __name__ == '__main__':
+    app.run(debug=True)
